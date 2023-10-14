9253f90a2818d12f0f03d56515c1d90609560a58
kenanargya <92387421+kenanargya@users.noreply.github.com>
2023-10-14 16:10:12 +0700

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using System;
+
+namespace Mobile_App
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            // Update button text
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            // Screen reader announcement
+            SemanticScreenReader.Announce(CounterBtn.Text);
+
+            // Button Click Animation
+            await CounterBtn.ScaleTo(1.1, 100);
+            await CounterBtn.ScaleTo(1.0, 100);
+
+            // Image Click Animation (Optional)
+            await this.FindByName<Image>("YourImageName").RelRotateTo(360, 1000);
+        }
+    }
+}
